61ccb6612fcdf6531a22a7d3206f7304eecd6d49
JoaoFelipe-rosa <joaofelipedarosa@gmail.com>
2022-12-14 15:51:41 -0300

    go in 0.02

---
+++ Softwares/install_cubic.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo "
+			================
+			INSTALANDO CUBIC
+			================
+"
+sudo apt-add-repository ppa:cubic-wizard/release -s
+
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6494C6D6997C215E -s
+
+sudo apt update && sudo apt install cubic -s
+
+echo "
+			===============
+			CUBIC INSTALADO
+			===============
+"
+clear
+
+echo "
+===========================
+Cubic Instalado Com Sucesso
+===========================
+"
