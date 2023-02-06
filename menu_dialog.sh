5fb0780c727f9033f2e2920a3bfd2516fb70c1c4
kevinkirst <kevin.kmk@hotmail.com>
2023-02-06 11:42:02 -0300

    created menu_dialog.sh

---
+++ menu_dialog.sh
@@ -0,0 +1,1 @@
+dialog --clear --backtitle "ZOOMtecnologia ltda" --title "Menu WinPEx64" --menu "escolha a opção desejada" 15 40 4 \ 1 "Atualizar BIOS" \ 2 "Inserir AssetTag" \ 3 "Instalar Windows" \ 4 "Ajuda" \ 5 "Reiniciar" \ 6 "Sair"
